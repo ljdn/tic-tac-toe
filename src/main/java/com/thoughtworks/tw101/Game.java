e8d0c1efd58c7c774e0b92317a37647786243343
lduan <lejia.duan@gmail.com>
2016-09-29 14:49:31 -0500

    Extracted playerMove() method from game; handles asking for input and redrawing game board

--- src/main/java/com/thoughtworks/tw101/Game.java
+++ src/main/java/com/thoughtworks/tw101/Game.java
@@ -19,6 +19,10 @@ public class Game {
     public void start() {
         printStream.println("1|2|3\n-----\n4|5|6\n-----\n7|8|9");
         printStream.println("Player 1: Make a move");
+        playerMove();
+    }
+
+    private void playerMove() {
         try {
             String move = bufferedReader.readLine();
             switch (move) {
