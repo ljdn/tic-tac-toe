ef04dcd7d99f190502a76a9ab7ca0194fdf621eb
lduan <lejia.duan@gmail.com>
2016-10-02 11:19:41 -0500

    Starting over!

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import com.thoughtworks.tw101.Game;
+
+/**
+ * Created by lduan on 10/2/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Game game = new Game();
+    }
+}
